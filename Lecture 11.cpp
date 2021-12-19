37804574879f05d60572706d6cb0f7875c9aee88
ClaudeLVU18 <84764547+ClaudeLVU18@users.noreply.github.com>
2021-12-19 22:25:26 +0400

    Create Lecture 11.cpp

---
+++ Lecture 11.cpp
@@ -0,0 +1,52 @@
+
+
+//risefallwhilestar
+
+void risefallwhileloopstar()
+{
+    int x=0;
+    while (x<= 5) 
+    {
+        x++;
+        int y=x;
+        while (y<=5)
+        {
+            y++;
+            cout << "*";
+        }
+        cout << endl;
+    }
+  
+ //table loop
+  void cinfail()
+{
+    int y;
+    cout << "ENTER A NUMBER YOU WANT THE TABLE OF: ";
+    cin >> y;
+
+    while (cin.fail()) {
+        cout << "INVALID COMMAND ENTER THE NUMBER AGAIN: " << endl;
+        cin.clear();
+        
+        cin >> y;
+    }
+
+    for (int x = 1; x <= 10; x++) {
+        cout << y << "x" << x <<" = " << y * x << endl;
+    }
+
+}
+
+ // factorial
+  
+  void factorial()
+{
+    cout<<"Enter a number to find its factorial";
+    int x;
+    cin >> x;
+    
+    for (int y = x; x>0; x--)
+    {
+        cout<< y * x;
+    }
+}
