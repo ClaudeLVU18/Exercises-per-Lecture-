f1fcdd12ef48ba53b1068bc4142c3916510bbcf9
ClaudeLVU18 <84764547+ClaudeLVU18@users.noreply.github.com>
2021-12-19 21:55:57 +0400

    Update Lecture 9.cpp

--- Lecture 9.cpp
+++ Lecture 9.cpp
@@ -1,3 +1,9 @@
+#include <iostream> //allows for variables to be loaded.
+#include <string> // allows string to be used.
+#include <stdlib.h> // allows for "system("CLS")" to be used to clear the console.
+#include <cmath>    // includes math functions onto the program.
+
+using namespace std;
 //REMAIN POSITIVE 
 
 void remainPositive()
@@ -57,4 +63,130 @@ void L9pass()
 
 }
 
-//POINTLESS BOX
+//POINTLESS BOX 
+void oneortwoloop()
+{
+    cout << "Enter either the number 1 or 2\n";
+    int input; 
+    cin >> input;
+    while (input != 0&&!cin.fail()&&input <= 2)
+    {
+        if (input == 1)
+        {
+            cout << "You have entered: 1\n";
+        }
+        else
+        {
+            cout << "You have entered: 2\n";
+        }
+
+        cout << "Enter either the number 1 or 2\n";
+        cin >> input;
+     
+    }
+cout << "Error: You have not entered neither the number 1 or 2";
+
+}
+
+//Pointless Box Never ending loop
+void pointlessboxloop()
+{
+    {
+    cout << "Enter either the number 1 or 2: ";
+    string input; 
+    cin >> input;
+    while (true)
+    {
+        if (input == "1")
+        {
+            cout << "You have entered: 1\n";
+        }
+        else if (input == "2")
+        {
+            cout << "You have entered: 2\n";
+        }
+        else
+        {
+            cout << "Error: You have not entered neither the number 1 or 2";
+            cout << "\nEnter either the number 1 or 2: ";
+            cin >> input;
+        }
+
+
+     
+    }
+
+
+}
+}
+
+//Reverse subtract by 9
+void reverse9()
+{
+    int num=108;
+    while (num>=9 ) 
+    {
+        
+        cout<<num<<endl;
+        num = num - 9;
+              
+    }
+    cin.get(); 
+    return 0;
+}
+
+//Loopy
+void countloop()
+{
+
+	int myInt = 0, input;
+	cout << "Enter a number: ";
+	cin >> input;
+    while (cin.fail())
+	{
+		cout << "Enter a number: ";
+		cin.clear();
+		cin.ignore(1000, '\n');
+		cin >> input;
+	}
+	do
+	{
+		cout << myInt << endl;
+		myInt++;
+
+	} while (myInt<=input);
+
+}
+
+//remain positive
+void remainpositive()
+{
+	cout<<"Enter a number to be subtracted by .5 'til 0: ";
+	double myNum;
+    cin >>myNum;
+	
+	while (myNum>0) {
+		myNum = myNum - 0.5;
+		cout << myNum << endl;
+	}
+	
+}
+
+//dowhileloop Quit
+void L9dowhileloop()
+{
+    char input;
+    do
+    {
+        cout << "\nWould you like to Quit (Y/N)?" <<
+        endl;
+        cin >> input;
+    }
+
+    while( (input != 'Y') && (input != 'y') );
+    
+
+    cout <<"Thanks for playing!";
+}
+    
+
